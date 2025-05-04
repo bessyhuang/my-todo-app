b434f340e3259afcef54d31866e8dd7dad71b8c8
bessyhuang <bessyhuang1@gmail.com>
2025-05-04 23:06:12 +0800

    feat: build todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo" />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.completed" />
+        <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { text: 'Learn Vue.js', completed: false },
+        { text: 'Build a To-Do App', completed: false }
+      ]
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() === '') return;
+      this.todos.push({
+        text: this.newTodo,
+        completed: false
+      });
+      this.newTodo = '';
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.completed {
+  text-decoration: line-through;
+}
+input[type="checkbox"] {
+  margin-right: 10px;
+}
+button {
+  margin-left: 10px;
+  color: red;
+  background: none;
+  border: none;
+  cursor: pointer;
 }
 </style>
+
